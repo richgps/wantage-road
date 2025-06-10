979b6807120ea8d4bd287f38275c3d3484063baf
Richard Ganpatsingh <richard.ganpatsingh@intelligenthealth.co.uk>
2025-06-10 20:42:57 +0100

    Show latest blog posts first

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,8 +1,16 @@
 import {defineQuery} from 'next-sanity'
 
-export const POSTS_QUERY = defineQuery(`*[_type == "post" && defined(slug.current)][0...3]{
-  _id, title, slug, mainImage, publishedAt, body
-}`)
+export const POSTS_QUERY = defineQuery(`
+  *[_type == "post" && defined(slug.current)]
+    | order(publishedAt desc)[0...3]{
+      _id,
+      title,
+      slug,
+      mainImage,
+      publishedAt,
+      body
+    }
+`)
 
 export const POST_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
   title, body, mainImage
